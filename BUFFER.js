952fbc392aadce2186a65600a27786c2db9fdf18
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2022-10-03 17:20:59 +0300

    Changed: Make storage of input characters closed. Avaiable only methods, no data itself.

--- BUFFER.js
+++ BUFFER.js
@@ -1,31 +1,7 @@
 import createBuffer from "./functions/createBuffer.js";
 
 
-export default
-{
-	value: "",
-	x: 0,
-	y: 0,
-	toString: function() 
-	{
-		return this.value;
-	},
-	clear: function() 
-	{
-		this.value = "";
-		return this;
-	},
-	add: function(value) 
-	{
-		this.value += String(value);
-		return this;
-	},
-	change: function(value) 
-	{
-		this.value = value;
-		return this;
-	},
-	get: function() {
-		return this.value;
-	},
-};
+const buffer = createBuffer();
+
+
+export default buffer;
